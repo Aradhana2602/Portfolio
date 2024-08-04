adfeb7d4ab06bcb9c8d925440ca72f2608ec2d91
Aradhana Kumari <aradhanakumari2602@gmail.com>
2024-08-04 11:37:05 +0530

    Activated download link

--- script.js
+++ script.js
@@ -89,6 +89,18 @@ srRight.reveal('.skills-box',{delay: 100})
 srRight.reveal('.form-control',{delay: 100})
 
 
+        // JavaScript function to handle the download
+        function downloadCV() {
+          const cvFilePath = 'https://drive.google.com/uc?export=download&id=1Y5Ql46FrQSDupYg4gAcss-P5ysSqGVsi'; // Link for direct download
+
+          const link = document.createElement('a');
+          link.href = cvFilePath;
+          link.download = 'YourName_CV.pdf'; // Optional: Rename the file during download
+          document.body.appendChild(link);
+          link.click();
+          document.body.removeChild(link);
+      }
+
 
 /* ----- CHANGE ACTIVE LINK ----- */
 
